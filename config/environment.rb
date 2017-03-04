dc9b8c5b798d054a223179c853c593f26d71b54f
pmp-2 <er7146@hotmail.com>
2017-03-04 04:54:43 +0000

    update5

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.assets.compile = true
-config.serve_static_assets = true
+#config.assets.compile = true
+#config.serve_static_assets = true
