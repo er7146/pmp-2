fe602342c53aaffe6b3cc37bef1934643d27df9f
pmp-2 <er7146@hotmail.com>
2017-04-04 06:03:43 +0000

    v40

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -22,13 +22,13 @@ attachment_config = {
   s3_credentials: {
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket:            ENV['S3_01valde']
+    bucket:            ENV['01valde']
   },
 
   storage:        :s3,
   s3_headers:     { "Cache-Control" => "max-age=31557600" },
   s3_protocol:    "https",
-  bucket:         ENV['S3_o1valde'],
+  bucket:         ENV['o1valde'],
   url:            ":s3_domain_url",
 
   styles: {
