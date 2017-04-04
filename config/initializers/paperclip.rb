ee4d4c495b5fe9e92d61c0a7a5e27d6afc227e49
pmp-2 <er7146@hotmail.com>
2017-04-04 05:38:22 +0000

    v38

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':01valde.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:ARN] = 'arn:aws:s3:::01valde'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
