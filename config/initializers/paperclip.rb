7de39d207a2127b37ee027f79e615110c2562816
pmp-2 <er7146@hotmail.com>
2017-05-02 06:27:14 +0000

    v66

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
 #Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_01valde] = 's3-us-east-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_01valde] = 's3-us-east-1.amazonaws.com'
