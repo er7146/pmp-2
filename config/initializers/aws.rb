fd35eeb19a2ceec73dfb4c37ce33f47de0bb577e
pmp-2 <er7146@hotmail.com>
2017-05-06 21:11:04 +0000

    v79

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -6,10 +6,5 @@ Aws.config.update({
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['AWS_S3_BUCKET'])
 
-# Get an instance of the S3 interface using the specified credentials configuration.
-s3 = Aws::S3.new()
 
-# Get a list of all object keys in a bucket.
-bucket = s3.buckets[bucket_name].objects.collect(&:key)
-puts bucket
 
