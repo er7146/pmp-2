04c79fcdad8412a9371bf676313b4e8bf2127610
pmp-2 <er7146@hotmail.com>
2017-06-15 21:13:10 +0000

    v97

--- app/overrides/white_label/upload_files.rb
+++ app/overrides/white_label/upload_files.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path  => "spree/checkout/_address",
                      :insert_top    => "[data-hook='shipping_fieldset_wrapper']",
 #                    :insert_top    => "erb[loud]:contains('div#homepage_sidebar_navigation')",
- #                    :text          => "<h2> It's a beginning </h2>",
+#                     :text          => "<b> Upload your files </b>",
                      :name          => "upload_files",
                      :partial       => "spree/shared/upload_files",
                      :original      => '9490d82472ffd92373e04f18fd0975de4bea3819')
