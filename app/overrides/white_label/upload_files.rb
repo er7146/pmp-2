f13f3374b1b6d5f0d7b5bff376fa9dfe302385b5
pmp-2 <er7146@hotmail.com>
2017-06-20 18:37:11 +0000

    v98

--- app/overrides/white_label/upload_files.rb
+++ app/overrides/white_label/upload_files.rb
@@ -5,10 +5,3 @@ Deface::Override.new(:virtual_path  => "spree/checkout/_address",
                      :name          => "upload_files",
                      :partial       => "spree/shared/upload_files",
                      :original      => '9490d82472ffd92373e04f18fd0975de4bea3819')
-
-
-#Deface::Override.new(:virtual_path => "spree/taxons/_taxon",
-#                     :replace      => "[data-hook='taxon']",
-#                     :name         => "taxon",
-#                     :partial      => "white_label/taxons/taxon",
-#                     :original     => '9490d82472ffd92373e04f18fd0975de4bea3819')
